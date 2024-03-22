bdd8fc2e3e8e2830b51a5e531bf49e95b0701dd3
정진욱 <likearome@gmail.com>
2024-03-22 10:52:56 +0900

    Graceful Shutdown을 위한 2F 인터럽트 추가 후킹. 2F-1510에 대해 인터럽트 대신 2F를 직접 콜

--- CD_RESID.C
+++ CD_RESID.C
@@ -6,6 +6,8 @@
 
 #include "FMDRVDEF.H"
 #include "CDROMDRV.H"
+#include "DOSUTIL.H"
+#include "INTERUP.H"
 
 // struct는 int등에 대해 바이트 얼라인을 맞추기 때문에,
 // 인터럽트 콜을 위해서는 반드시 바이트 얼라인을 1로 맞춰주어야 한다.
@@ -63,14 +65,13 @@ typedef struct
 // 이 코드는 메모리에 남는다.
 #pragma code_seg(BEGTEXT, CODE)
 
-/* zero out an object of l bytes */
-static void zerobytes(void* obj, unsigned short l) {
-	unsigned char* o = obj;
-	while (l-- != 0) {
-		*o = 0;
-		o++;
-	}
-}
+//static void zerobytes(void* obj, unsigned short l) {
+//	unsigned char* o = obj;
+//	while (l-- != 0) {
+//		*o = 0;
+//		o++;
+//	}
+//}
 
 // ======== Utility Functions ========
 static int8 CallCDROMDriver(int16 drive, uint8 command, void far* ptr, uint32 size, uint16* status)
@@ -79,7 +80,9 @@ static int8 CallCDROMDriver(int16 drive, uint8 command, void far* ptr, uint32 si
 	void far* reqHdrPtr = &reqHeader;
 	unsigned short reqHdrPtrSeg = _FP_SEG(reqHdrPtr);
 	unsigned short reqHdrPtrOff = _FP_OFF(reqHdrPtr);
-	char isFail = 0;
+	bool isFail = 0;
+
+	void (__interrupt __far * oldMPXInt)() = _MK_FP(globData.prev_mpx_handler_seg, globData.prev_mpx_handler_off);
 
 	//union REGS cdrom;
 	//struct SREGS scdrom;
@@ -103,8 +106,13 @@ static int8 CallCDROMDriver(int16 drive, uint8 command, void far* ptr, uint32 si
 		mov es, reqHdrPtrSeg
 		mov bx, reqHdrPtrOff
 
+		jmp CDROMDRVSIG
+		CDSIG db 'CDDR'
+		CDROMDRVSIG:
+
 		mov ax, 1510h
-		int 2Fh
+		pushf
+		call dword ptr oldMPXInt
 
 		jnc CFLAG_SUCCESS
 		mov isFail, 1
@@ -135,7 +143,6 @@ static int8 CallCDROMDriver(int16 drive, uint8 command, void far* ptr, uint32 si
 int8 CDAudio_SetVolume(int8 INPARAM drive, uint8 INPARAM volume)
 {
 	AudioChannelSet channelInfo;
-	zerobytes(&channelInfo, sizeof(channelInfo));
 	channelInfo.notUsed = 3;        //subfunc
 	channelInfo.outChannel0 = 0;
 	channelInfo.outChannel1 = 1;
@@ -146,19 +153,36 @@ int8 CDAudio_SetVolume(int8 INPARAM drive, uint8 INPARAM volume)
 	channelInfo.outVolume2 = volume;
 	channelInfo.outVolume3 = volume;
 
+	//_reset_cursor();
+	//_putchar('S'); _putchar('V'); _putchar('O'); _putchar('L'); _putchar('\n');
 	CallCDROMDriver(drive, CMD_IOCTL_OUT, &channelInfo, sizeof(channelInfo), 0);
+	//_putchar('O'); _putchar('K'); _putchar('\n');
 	return CDAUDIO_SUCCESS;
 }
 
 int8 CDAudio_PlaySector(int8 INPARAM drive, uint32 INPARAM startSector, uint32 INPARAM endSector)
 {
-	return CallCDROMDriver(drive, CMD_PLAY, (void far*)(startSector - TRACK_MARGIN), endSector - startSector, 0);
+	int8 ret;
+
+	//_reset_cursor();
+	//_putchar('P'); _putchar('L'); _putchar('A'); _putchar('Y'); _putchar('\n');
+	ret = CallCDROMDriver(drive, 
+		CMD_PLAY, 
+		startSector < TRACK_MARGIN ? (void far*)0 : (void far*)(startSector - TRACK_MARGIN),
+		endSector < startSector ? 0 : endSector - startSector,
+		0);
+	//_putchar('O'); _putchar('K'); _putchar('\n');
+	return ret;
 }
 
 int8 CDAudio_Stop(int8 INPARAM drive)
 {
-	int8 ret = CallCDROMDriver(drive, CMD_STOP, 0, 0, 0);
+	int8 ret;
+	//_reset_cursor();
+	//_putchar('S'); _putchar('T'); _putchar('O'); _putchar('P'); _putchar('\n');
+	ret = CallCDROMDriver(drive, CMD_STOP, 0, 0, 0);
 	ret |= CallCDROMDriver(drive, CMD_STOP, 0, 0, 0);
+	//_putchar('O'); _putchar('K'); _putchar('\n');
 	return ret;
 }
 
@@ -167,12 +191,16 @@ int8 CDAudio_CheckCDBusy(int8 INPARAM drive)
 	AudioInfo audioInfo;
 	uint16 status;
 
-	zerobytes(&audioInfo, sizeof(audioInfo));
+	//_reset_cursor();
+	//_putchar('B'); _putchar('U'); _putchar('S'); _putchar('Y'); _putchar('\n');
+
 	audioInfo.subfunc = 15;
 	if (CDAUDIO_FAIL == CallCDROMDriver(drive, CMD_IOCTL_IN, &audioInfo, sizeof(audioInfo), &status))
 	{
+		//_putchar('F'); _putchar('A'); _putchar('I'); _putchar('L'); _putchar('\n');
 		return CDAUDIO_FAIL;
 	}
+	//_putchar('O'); _putchar('K'); _putchar('\n');
 
 	if (0 != (status & CD_BUSYBIT))
 	{
